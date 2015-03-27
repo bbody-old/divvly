91765e8e2f29b91838135afc8833ff83602f9295
Prelang Builder <builder@prelang.com>
2015-03-27 23:48:08 +0000

    Added require_user_signed_in filters to scaffolded controller

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -1,4 +1,8 @@
 class BillsController < ApplicationController
+
+  #->Prelang (scaffolding:rails/scope_to_user)
+  before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
+
   before_action :set_bill, only: [:show, :edit, :update, :destroy]
 
   # GET /bills
